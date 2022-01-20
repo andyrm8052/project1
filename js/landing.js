16a44a8b65f2971939bc6d0453e953b7b16ad8f6
andyrm8052 <andy1994>
2022-01-20 17:48:54 -0500

    TASK: Committed JS File and Images

---
+++ js/landing.js
@@ -0,0 +1,17 @@
+var slide = 0;
+carousel();
+
+function carousel(n){
+    var i;
+    var images = document.getElementsByClassName("carousel_img");
+    for (i = 0; i < images.length; i++){
+        images[i].style.display = "none";
+    };
+    slide++;
+    if (slide > images.length){
+        slide = 1;
+    }
+    images[slide - 1].style.display = "block";
+};
+
+setInterval(carousel, 2500);
